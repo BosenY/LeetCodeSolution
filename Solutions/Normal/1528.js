70f01b40d493e60a65efbce2c2c628f65e328951
BosenY <yxy43445@gmail.com>
2020-07-28 10:24:24 +0800

    feat: 1526 resolution

---
+++ Solutions/Normal/1528.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let strArr =  []
+    for(let i = 0 ; i < indices.length; i ++) {
+        strArr[indices[i]] = s[i] 
+    }
+    return strArr.join("")
+};
