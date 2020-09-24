5761978ea703c3a76ed6e4f9067a5f06f68c4318
BosenY <yxy43445@gmail.com>
2020-09-24 17:36:04 +0800

    feat: upgrade 501

--- Solutions/Normal/501.js
+++ Solutions/Normal/501.js
@@ -11,37 +11,29 @@
  * @return {number[]}
  */
 var findMode = function (root) {
-    if (!root) return []
+  if (!root) return [];
   let max = 0,
     res = [],
     preNum = undefined,
     num = 0;
-
   dfs(root);
-  console.log(res)
   return res;
   function dfs(root) {
     if (!root) return;
     dfs(root.left);
-
     if (preNum === root.val) {
       num++;
     } else {
       num = 1;
     }
-
     if (num === max) {
       res.push(root.val);
     } else if (num > max) {
       res = [root.val];
-
       max = num;
     }
-
     preNum = root.val;
 
     dfs(root.right);
   }
 };
-
-console.log(findMode([2147483647]))
